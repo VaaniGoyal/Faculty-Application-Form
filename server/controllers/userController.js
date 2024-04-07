edd89c8d524fd93b009859fb061fe2abd93754c1
VaaniGoyal <vanu.goyal@gmail.com>
2024-04-07 12:48:00 +0530

    tested backend api using postman

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,21 @@
+//userController.js
 const { hashSync } = require("bcryptjs");
-const User = require('../models/user');
+const User = require('../models/user'); // Import Sequelize model
+
+async function createUser(req, res) {
+  try {
+    const { name, password } = req.body;
+    if (typeof name !== 'string' || typeof password !== 'string') {
+      throw new Error('Name and password must be strings');
+    }
+    const hashedPassword = hashSync(password, 10);
+    const newUser = await User.create({ name, password: hashedPassword });
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ message: "Failed to create user." });
+  }
+}
 
 async function getUser(req, res) {
   try {
@@ -8,18 +24,6 @@ async function getUser(req, res) {
   } catch (error) {
     console.error("Error fetching users:", error);
     return res.status(500).json({ message: "Failed to fetch users." });
-  }
-}
-
-async function createUser(req, res) {
-  try {
-    const { name, password } = req.body;
-    const hashedPassword = hashSync(password, 10);
-    const newUser = await User.create({ name, password: hashedPassword });
-    return res.status(201).json(newUser);
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return res.status(500).json({ message: "Failed to create user." });
   }
 }
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,21 +1,11 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('../controllers/userController.js');
-
-// // Route for creating a user
-// router.post('/createUser', userController.createUser);
-// router.get('/', userController.getUser);
-// module.exports = function (app) {
-//     app.use("/api/users", router);
-//   };
-
+//userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Routes
-router.get('/', userController.getUser);
-router.post('/', userController.createUser);
+router.get('/getUser', userController.getUser);
+router.post('/createUser', userController.createUser);
 
 module.exports = router;
 
