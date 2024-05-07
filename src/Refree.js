49f262d7ad384f1ee3cb7762792a68494aa26a3f
Vaishika05 <agrawalvaishika5@gmail.com>
2024-05-07 23:41:59 +0530

    Refree-Styling ka final kal kar dungi

--- src/Refree.js
+++ src/Refree.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const RefreeForm = () => {
-    const app_number=localStorage.getItem("app_number");
+const RefereeForm = () => {
+    const app_number = localStorage.getItem("app_number");
     const name = localStorage.getItem("name");
     const navigate = useNavigate();
-    
-    /*-------------------------------------REFREES---------------------------------------------- */
+
     const [formData, setFormData] = useState({
         formDetails: [
             {
@@ -18,15 +17,33 @@ const RefreeForm = () => {
                 organisation: "",
                 email: "",
                 contact: "",
+                phdCert: null,
+                pgDoc: null,
+                ugDoc: null,
+                hsc12Doc: null,
+                hsc10Doc: null,
+                payslipDoc: null,
+                nocDoc: null,
+                postPhdExpDoc: null,
+                signature: null
             }
         ]
     });
+
     const handleInputChange = (e, index) => {
         const { name, value } = e.target;
         const updatedData = [...formData.formDetails]
         updatedData[index][name] = value;
         setFormData({ ...formData, formDetails: updatedData });
     };
+
+    const handleFileChange = (e, index, fileType) => {
+        const { files } = e.target;
+        const updatedData = [...formData.formDetails];
+        updatedData[index][fileType] = files[0];
+        setFormData({ ...formData, formDetails: updatedData });
+    };
+
     const handleAddMore = () => {
         setFormData({
             ...formData,
@@ -40,83 +57,174 @@ const RefreeForm = () => {
                     organisation: "",
                     email: "",
                     contact: "",
+                    phdCert: null,
+                    pgDoc: null,
+                    ugDoc: null,
+                    hsc12Doc: null,
+                    hsc10Doc: null,
+                    payslipDoc: null,
+                    nocDoc: null,
+                    postPhdExpDoc: null,
+                    signature: null
                 }
             ]
         });
     };
+
     const handleSubmit = async () => {
         try {
-            // Iterate over each row in additionalDetails array
             for (let i = 0; i < formData.formDetails.length; i++) {
                 const row = formData.formDetails[i];
-                // Send a POST request for each row
-                const response = await axios.post("http://localhost:3000/api/other/addReferees", row);
+                const formDataToSend = new FormData();
+                Object.entries(row).forEach(([key, value]) => {
+                    formDataToSend.append(key, value);
+                });
+                const response = await axios.post("http://localhost:3000/api/other/addReferees", formDataToSend);
                 console.log(response.data);
             }
-            // navigate('/New_page').....final checkbox wala
+            // Uncomment and implement navigation logic here
+            // navigate('/New_page');
         } catch (error) {
-            console.error("Error submitting refree details :", error);
+            console.error("Error submitting referee details:", error);
         }
     };
-    const handleLogout = async (e) => {
+
+    const handleLogout = async () => {
         navigate('/Login_Page');
     }
-/*------------------------------------------------------------------------------------------------------------------- */
+
     return (
         <div className="Present_Employment" style={{ marginTop: '12rem', marginLeft: '7rem', marginRight: '7rem', marginBottom: '4rem', backgroundColor: '#f5f5f5' }}>
-        <h2 style={{ animation: 'blinker 1s linear infinite', textAlign: 'center', color: '#d15f75' }}>Apply For Faculty Position</h2>
-        <h7> Welcome {name}!!</h7>  <button onClick={handleLogout}> Logout </button>
+            <h2 style={{ animation: 'blinker 1s linear infinite', textAlign: 'center', color: '#d15f75' }}>Apply For Faculty Position</h2>
+            <h7> Welcome {name}!!</h7>  <button onClick={handleLogout}> Logout </button>
 
-        
-
-
-       {/* write  code for pdf uploads */}
-
-
-
-
-
-
-        {/* Refree Form */}
-        <form onSubmit={handleSubmit} id="RefreeForm">
+            <form onSubmit={handleSubmit} id="RefereeForm">
                 <fieldset style={{ padding: '1rem', marginBottom: '0.5rem' }}>
-                <legend style={{ backgroundColor: '#e1f0d8', color: '#54773c', padding: '0.5rem', borderRadius: '0.5rem', width: '100%', fontWeight: 'bold' }}>Refrees *</legend>
-                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Association with Refree</th>
-                        <th>Organization/Institution</th>
-                        <th>e-mail</th>
-                        <th>Contact No.</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {formData.formDetails.map((row, index) => (
-                    <tr key = {index}>
-                        <td><input type="text" name="name" onChange={(e) => handleInputChange(e, index)} value={row.name} required /></td>
-                        <td><input type="text" name="position" onChange={(e) => handleInputChange(e, index)} value={row.position} required /></td>
-                        <td><input type="text" name="association" onChange={(e) => handleInputChange(e, index)} value={row.association} required /></td>
-                        <td><input type="text" name="organization" onChange={(e) => handleInputChange(e, index)} value={row.organisation} required /></td>
-                        <td><input type="text" name="email" onChange={(e) => handleInputChange(e, index)} value={row.email} required /></td>
-                        <td><input type="text" name="contact" onChange={(e) => handleInputChange(e, index)} value={row.contact} required /></td>
-                    </tr>
-                ))}
-                </tbody>
-                </table>
-                <button onClick={handleAddMore}>Add More</button>
-                <button type="button" onClick={handleSubmit}>Save </button>
+                    <legend style={{ backgroundColor: '#e1f0d8', color: '#54773c', padding: '0.5rem', borderRadius: '0.5rem', width: '100%', fontWeight: 'bold' }}>Referees *</legend>
+                    <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Association with Referee</th>
+                                <th>Organization/Institution</th>
+                                <th>e-mail</th>
+                                <th>Contact No.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {formData.formDetails.map((row, index) => (
+                                <tr key={index}>
+                                    <td><input type="text" name="name" onChange={(e) => handleInputChange(e, index)} value={row.name} required /></td>
+                                    <td><input type="text" name="position" onChange={(e) => handleInputChange(e, index)} value={row.position} required /></td>
+                                    <td><input type="text" name="association" onChange={(e) => handleInputChange(e, index)} value={row.association} required /></td>
+                                    <td><input type="text" name="organisation" onChange={(e) => handleInputChange(e, index)} value={row.organisation} required /></td>
+                                    <td><input type="text" name="email" onChange={(e) => handleInputChange(e, index)} value={row.email} required /></td>
+                                    <td><input type="text" name="contact" onChange={(e) => handleInputChange(e, index)} value={row.contact} required /></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <button onClick={handleAddMore}>Add More</button>
+                    <button type="button" onClick={handleSubmit}>Save </button>
                 </fieldset>
-        </form>
+                <fieldset>
+                    <legend style={{ backgroundColor: '#e1f0d8', color: '#54773c', padding: '0.5rem', borderRadius: '0.5rem', width: '100%', fontWeight: 'bold' }}>Upload The Following Documents*</legend>
+                    {formData.formDetails.map((row, index) => (
+                        <div key={index} style={{ marginBottom: '1rem' }}>
+                             <div style={{ border: '2px solid black' }}>
+                             <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD PHD CERTIFICATE</h3>
+                                <p style={{color:'#43862a'}}> PHD Certificate</p>
+                                <div>
+                                    
+                                    <input type="file" accept=".pdf" name="phdCert" onChange={(e) => handleFileChange(e, index, 'phdCert')} required />
+                                    {row.phdCert && <a href={URL.createObjectURL(row.phdCert)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                          
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD PG CERTIFICATE</h3>
+                                
+                                <p style={{color:'#43862a'}}>PG Documents</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="pgDoc" onChange={(e) => handleFileChange(e, index, 'pgDoc')} required />
+                                    {row.pgDoc && <a href={URL.createObjectURL(row.pgDoc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD UG CERTIFICATE</h3>
+                                <p style={{color:'#43862a'}}>UG Documents</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="ugDoc" onChange={(e) => handleFileChange(e, index, 'ugDoc')} required />
+                                    {row.ugDoc && <a href={URL.createObjectURL(row.ugDoc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD 12th/HSC/Diploma Documents</h3>
+                                <p style={{color:'#43862a'}}>12th/HSC/Diploma Documents</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="hsc12Doc" onChange={(e) => handleFileChange(e, index, 'hsc12Doc')} required />
+                                    {row.hsc12Doc && <a href={URL.createObjectURL(row.hsc12Doc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD 10th/SSC Documents</h3>
+                                <p style={{color:'#43862a'}}>10th/SSC Documents</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="hsc10Doc" onChange={(e) => handleFileChange(e, index, 'hsc10Doc')} required />
+                                    {row.hsc10Doc && <a href={URL.createObjectURL(row.hsc10Doc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD PAY SLIP</h3>
+                                <p style={{color:'#43862a'}}>Pay Slip</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="payslipDoc" onChange={(e) => handleFileChange(e, index, 'payslipDoc')} required />
+                                    {row.payslipDoc && <a href={URL.createObjectURL(row.payslipDoc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD NOC Documents</h3>
+                                <p style={{color:'#43862a'}}>Undertaking-in case, NOC is not available at the time of application but will be provided at the time of interview</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="nocDoc" onChange={(e) => handleFileChange(e, index, 'nocDoc')} required />
+                                    {row.nocDoc && <a href={URL.createObjectURL(row.nocDoc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD POST PHD CERTIFICATE</h3>
+                                <p style={{color:'#43862a'}}>Post phd Experience Certificate/All Experience Certificates/ Last Pay slip</p>
+                                <div>
+                                    <input type="file" accept=".pdf" name="postPhdExpDoc" onChange={(e) => handleFileChange(e, index, 'postPhdExpDoc')} required />
+                                    {row.postPhdExpDoc && <a href={URL.createObjectURL(row.postPhdExpDoc)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                              {/* Add signature upload block */}
+                              <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD Signature</h3>
+                                <p style={{color:'#43862a'}}>Upload your Signature in JPG only</p>
+                                <div>
+                                    <input type="file" accept=".jpg" name="signature" onChange={(e) => handleFileChange(e, index, 'signature')} required />
+                                    {row.signature && <a href={URL.createObjectURL(row.signature)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                            {/* End of signature upload block */}
+
+                            <div style={{ border: '2px solid black' }}> 
+                            <h3 style={{color: '#42708f', backgroundColor:'#ddedf7'}}> UPLOAD ANY OTHER DOCUMENT</h3>
+                                <p style={{color:'#43862a'}}>Upload any other relevant document in a single PDF (For example award certificate, experience certificate etc) . If there are multiple documents, combine all the documents in a single PDF less than 1MB.</p>
+                                <div>
+                                    <input type="file" accept=".jpg" name="signature" onChange={(e) => handleFileChange(e, index, 'signature')} required />
+                                    {row.signature && <a href={URL.createObjectURL(row.signature)} target="_blank" rel="noreferrer">View Uploaded File</a>}
+                                </div>
+                            </div>
+                        </div>
+                        
+                    ))}
+                </fieldset>
+            </form>
         </div>
     );
 }
 
-export default RefreeForm;
-
-
-
-
-
-
+export default RefereeForm;
