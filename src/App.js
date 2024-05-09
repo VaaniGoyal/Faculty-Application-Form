b4beb20da2d747773489b3f77e41e1e835c6d347
VaaniGoyal <vanu.goyal@gmail.com>
2024-05-09 18:12:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Footer />
       <div>
         <Routes>
-          <Route path="/" element={<Contribution />} />
+          <Route path="/" element={<Login_Page />} />
           <Route path="/Login_Page" element={<Login_Page />} />
           <Route path="/Create_Account" element={<Create_Account />} />\
           <Route path="/Personal_Details" element={<Personal_Details />} />
